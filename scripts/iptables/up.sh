132fabda902c9699ff95b85d77f71e9629e6226d
Jason Gallavin <jasongallavin@gmail.com>
2017-03-02 23:33:41 -0500

    fix service command

--- scripts/iptables/up.sh
+++ scripts/iptables/up.sh
@@ -1,1 +1,1 @@
-#!/usr/bin/env bashiptables -A INPUT -p tcp --dport ssh -j ACCEPTiptables -A INPUT -p tcp --dport http -j ACCEPTiptables -I INPUT 1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPTiptables -P INPUT DROPiptables service iptables save
+#!/usr/bin/env bashiptables -A INPUT -p tcp --dport ssh -j ACCEPTiptables -A INPUT -p tcp --dport http -j ACCEPTiptables -I INPUT 1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPTiptables -P INPUT DROPservice iptables save
